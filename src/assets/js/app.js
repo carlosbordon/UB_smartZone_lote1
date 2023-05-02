e387f85ca945bc965b715ec8547589b20e2b5498
Carlos Bordón <carlosbordon@icloud.com>
2023-05-02 15:19:08 +0200

    totop

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,39 @@
+
+// Header hidden
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("sticky").style.top = "0";
+  } else {
+    document.getElementById("sticky").style.top = "-500px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+// Totop
+
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#toTop').fadeIn();
+			} else {
+				$('#toTop').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#toTop').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 100);
+			return false;
+		});
+});
+
+
+
 //VARIABLES
 let screenWidth = screen.width
 let detail = document.getElementById("detail");
@@ -22,40 +58,6 @@ aprendizaje.style.display = "none";
 trabajo.style.display = "none";
 teorico_practica.style.display = "none";
 t_grupos.style.display = "none";
-// Hide Header on on scroll down
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("sticky").style.top = "0";
-  } else {
-    document.getElementById("sticky").style.top = "-500px";
-  }
-  prevScrollpos = currentScrollPos;
-}
-
-
-// Get the button
-let mybutton = document.getElementById("toTop");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 80) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 
 function openDetail() {
   if (screenWidth < 579) {
